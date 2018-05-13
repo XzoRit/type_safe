42c362bb10a8605bf54e7695f565e2be3809aa2a
XzoRit <xzorit@hotmail.com>
2018-05-13 12:30:53 +0200

    add example on how to use the type_safe::integer type

--- type_safe_test.cpp
+++ type_safe_test.cpp
@@ -1,16 +1,33 @@
 #include <type_safe/integer.hpp>
+#include <limits>
+
 #include <boost/test/unit_test.hpp>
+
+namespace ts = type_safe;
 
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE(type_safe)
 
-BOOST_AUTO_TEST_CASE(test_a)
+BOOST_AUTO_TEST_CASE(ts_integer)
 {
-    int a{1};
-    int b{2};
+    {
+        using ts_uint = ts::integer<unsigned>;
 
-    BOOST_TEST(a + b == 0);
+        const ts_uint a{1u};
+        const unsigned int b{2};
+
+        const ts_uint c = a + b;
+        BOOST_TEST(c == ts::make_unsigned(3));
+    }
+    {
+        using ts_uint = ts::integer<unsigned, ts::checked_arithmetic>;
+
+        const ts_uint a{numeric_limits<ts_uint::integer_type>::max() - 1};
+        const auto b{2u};
+
+        BOOST_REQUIRE_THROW(a + b, ts::checked_arithmetic::error);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
