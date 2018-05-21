98b2d630f28555c65ffb8213936ad4941e2344f8
XzoRit <xzorit@hotmail.com>
2018-05-21 15:53:31 +0200

    format sources with clang-format

--- type_safe_test.cpp
+++ type_safe_test.cpp
@@ -1,11 +1,11 @@
-#include <type_safe/integer.hpp>
-#include <type_safe/flag.hpp>
 #include <type_safe/constrained_type.hpp>
+#include <type_safe/flag.hpp>
+#include <type_safe/integer.hpp>
 
 #include <boost/core/demangle.hpp>
 
-#include <limits>
 #include <iostream>
+#include <limits>
 #include <type_traits>
 
 #include <boost/test/unit_test.hpp>
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_CASE(ts_integer)
     {
         using ts_uint = ts::integer<unsigned>;
 
-        const ts_uint a{1u};
+        const ts_uint      a{1u};
         const unsigned int b{2};
 
         const ts_uint c = a + b;
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(ts_integer)
         using ts_uint = ts::integer<unsigned, ts::checked_arithmetic>;
 
         const ts_uint a{numeric_limits<ts_uint::integer_type>::max() - 1};
-        const auto b{2u};
+        const auto    b{2u};
 
         BOOST_REQUIRE_THROW(a + b, ts::checked_arithmetic::error);
     }
@@ -61,38 +61,40 @@ BOOST_AUTO_TEST_CASE(ts_flag)
         BOOST_TEST(!a.try_reset());
     }
     {
-        vector nums = {1, 2, 1, 3, 4, 1, 5, 6, 7};
-        flag found_one{false};
+        vector      nums = {1, 2, 1, 3, 4, 1, 5, 6, 7};
+        flag        found_one{false};
         vector<int> nums_after_one{};
-        for(const auto& a : nums)
-        {
-            if(a == 1) found_one.change(true);
-            else if(found_one.try_reset()) nums_after_one.push_back(a);
+        for(const auto& a : nums) {
+            if(a == 1)
+                found_one.change(true);
+            else if(found_one.try_reset())
+                nums_after_one.push_back(a);
         }
         const vector expected{2, 3, 5};
-        BOOST_REQUIRE_EQUAL_COLLECTIONS(
-            begin(nums_after_one), end(nums_after_one),
-            begin(expected), end(expected));
+        BOOST_REQUIRE_EQUAL_COLLECTIONS(begin(nums_after_one), end(nums_after_one), begin(expected),
+                                        end(expected));
     }
 }
 
 struct boost_test_verifier
 {
-    template<class Value, class Constrain>
+    template <class Value, class Constrain>
     static constexpr Value verify(Value a, const Constrain& c)
     {
-        BOOST_TEST(c(a),
-                   "constraint: \"" << demangle(typeid(c).name()) << "\" not satisfied with value = " << a);
+        BOOST_TEST(c(a), "constraint: \"" << demangle(typeid(c).name())
+                                          << "\" not satisfied with value = " << a);
         return a;
     }
 };
 
 struct is_odd
 {
-    template<class Value>
-    struct is_valid : is_unsigned<Value> {};
+    template <class Value>
+    struct is_valid : is_unsigned<Value>
+    {
+    };
 
-    template<class Value>
+    template <class Value>
     constexpr bool operator()(Value a) const noexcept(noexcept(a % 2))
     {
         return a % 2;
